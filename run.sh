e35fe4e8490808707f4935c5cc885831b804d367
Nevada Sanchez <sanchez.nevada@gmail.com>
2017-11-28 00:45:24 -0500

    Make project work on Fedora based platforms as well.

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
+# Usage:
+#   ./run.sh [notebook]
+#
+# With `notebook`, it will launch a Jupyter session as sudo.
+# Otherwise it will run `scapy`.
+
+# Add pyenv to the shell
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# Run scapy
 if [[ $1 == "notebook" ]]; then
-    sudo jupyter notebook --allow-root
+    sudo -E $(echo $(which jupyter)) notebook --allow-root
 else
-    sudo scapy -c .scapy_startup.py
+    sudo -E $(echo $(which scapy)) -c .scapy_startup.py
 fi
